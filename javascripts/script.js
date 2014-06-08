748bfdac69d353a51dbc448fc341de2343311e0b
Arnaud Ferreri <ferreri@rnaud.net>
2014-06-07 19:42:20 -0700

    working animations

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,8 +1,49 @@
-Zepto(function($){
+$( document ).ready(function() {
   $(".tab-header li").on('click', function(e){
     target = $(this).data("target");
     tabs = $(this).parent().parent().find(".tabs-container");
-    tabs.find("div").removeClass("active");
-    tabs.find("div."+target).addClass("active");
+    tabs.find("div").removeClass("appeared");
+    tabs.find("div."+target).addClass("appeared");
   });
+
+  $(".menu li").on('click', function(e){
+    target = $(this).data("target");
+    div_target = $("#"+target);
+
+    console.log(div_target);
+
+    $('html,body').animate({
+      scrollTop: div_target.offset().top
+    }, 500);
+  });
+
+  jQuery(window).load(function() {
+    setTimeout(function(){
+      $(".logo img").addClass("appeared");
+    }, 800);
+
+    setTimeout(function(){
+      $(".logo .title").addClass("appeared");
+    }, 1200);
+  });
+
+  // $(".logo img").one('load', function() {
+
+  // });
+
+  $(".bg").each(function(index) {
+    bgDiv = $(this)
+    var bgImgSrc = bgDiv.css('background-image');
+    bgImgSrc = bgImgSrc.replace('url(','').replace(')','');
+
+    bgImg = new Image();
+    bgImg.div = bgDiv;
+    bgImg.onload = function(){
+      console.log("loaded");
+      this.div.addClass("appeared");
+    };
+    bgImg.src = bgImgSrc;
+
+  });
+
 });
