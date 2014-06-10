90ae07b9efc31c2bfc2ef37daf5858c4613da0f1
Ivan <ivan.abrossimov@gmail.com>
2014-06-10 12:05:58 +0200

    create selected work

--- javascripts/script.js
+++ javascripts/script.js
@@ -46,4 +46,12 @@ $( document ).ready(function() {
 
   });
 
+      $('.viewport').mouseenter(function(e) {
+        $(this).children('a').children('img').animate({ height: '299', left: '0', top: '0', width: '450'}, 100);
+        $(this).children('a').children('span').fadeIn(200);
+    }).mouseleave(function(e) {
+        $(this).children('a').children('img').animate({ height: '332', left: '-20', top: '-20', width: '500'}, 100);
+        $(this).children('a').children('span').fadeOut(200);
+    });
+
 });
